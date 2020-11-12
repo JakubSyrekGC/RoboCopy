177b4f4d6081fff3f4015cee7dded53ff3b2e6c5
Jakub Syrek <jaqb.syrek@gmail.com>
2020-11-12 20:22:10 +0100

    Add project files.

---
+++ Robo-Copy/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robo_Copy
+{
+    public class Class1
+    {
+    }
+}
